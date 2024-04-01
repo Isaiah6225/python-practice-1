f14cacc69c434a2d8416e70eef8b6980fa9e1b81
Isaiah6225 <isaiahjohnson6225@gmail.com>
2024-04-01 15:08:14 -0400

    Created a test project to interact with the CLI using the sys import

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import hashlib
+import sys
 import getpass
 
 
@@ -7,8 +8,27 @@ def hashPassword(x):
     hash.update(x.encode('utf-8'))
     print(hash.hexdigest())
 
-userInput = getpass.getpass("Enter your password: ")
-hashPassword(userInput)
+
+help_message =  "Usage: python3 main.py [arg] Example: python3 main.py -d Command list: -hA(sha256 hash"
+
+if sys.argv[1] == "-hA":
+    if len(sys.argv) < 4:
+        userPass = getpass.getpass("Please enter the password: ")
+        hashPassword(userPass)
+        sys.exit(1)
+
+elif sys.argv[1] == "-d":
+    pass
+
+elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    print(help_message)
+
+else: 
+    print("First argument no found " + sys.argv[1])    
+    print(help_message)
+
+
+
 
 
 
