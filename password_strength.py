857b8c9ed05b0d4f7044b402636997f47f1915b8
Isaiah6225 <isaiahjohnson6225@gmail.com>
2024-04-09 15:37:29 -0400

    Finish the program to check the password strength of the users password

--- password_strength.py
+++ password_strength.py
@@ -1,18 +1,62 @@
-import sys
-import string 
+
 
 def check_strength(user_pass):
-    for char in user_pass:
-        if(char.isupper()):
-            letters_uc = char 
-            print(letters_uc)
+    letters_uc = []
+    letters_lc = []
+    non_letters = []
+    lc_strength_number =0
+    nc_strength_number =0 
+    uc_strength_number =0
 
-        elif(char.islower()):
-            letters_lc = char 
-            print(letters_lc)
-        elif():
-            non_letter = char
-            print(non_letter)
+    for char in user_pass:
+      
+        if char.isupper():
+            
+            letters_uc.append(char)
+            
+            if( 2 > len(letters_uc) < 6):
+                uc_strength_number = 1
+                
+            elif(len(letters_uc) < 2):
+                uc_strength_number =0
+
+            elif(len(letters_uc) >= 6):
+                uc_strength_number = 2
+
+        elif char.islower():
+
+            letters_lc.append(char)
+
+            if( 2 > len(letters_lc) < 6):
+                lc_strength_number = 1
+                
+            elif(len(letters_lc) < 2):
+                lc_strength_number =0
+
+            elif(len(letters_lc) >= 6):
+                lc_strength_number = 2
+        
+        else: 
+            
+            non_letters.append(char)
+
+            if( 2 > len(non_letters) < 6):
+                nc_strength_number = 1
+                
+            elif(len(non_letters) < 2):
+                nc_strength_number =0
+
+            elif(len(non_letters) >= 6):
+                nc_strength_number = 2
+
+    total_strength = (uc_strength_number + lc_strength_number + nc_strength_number)
+    if(total_strength == 6):
+        print("Strength: high!")
+    elif( 4 < total_strength > 6):
+        print("Strength: medium")
+    elif(total_strength <= 2):
+        print("Strength: low")
+
 
 def check_password(user_pass):
     if(len(user_pass) >= 12):
