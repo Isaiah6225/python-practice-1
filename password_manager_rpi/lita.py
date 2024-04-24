61b9a95ecab2d662ef54c83f93e9d7533bb0e03a
Isaiah6225 <isaiahjohnson6225@gmail.com>
2024-04-23 20:29:51 -0400

    Will update soon

--- password_manager_rpi/lita.py
+++ password_manager_rpi/lita.py
@@ -1,13 +1,21 @@
 import sys
 
-# this class will be the encryption alogrithm for the password manager
-def format_data(user_pass, pass_purpose, pass_username):
-
-    pass_info = f"{user_pass} {pass_purpose} {pass_username}"
-
-    pass_info_bits = sys.getsizeof(pass_info) * 8
+user_pass = 'abcdefGHIJKL1234@#'
+pass_purpose = 'Google'
+pass_username = 'Isaiah6225'
+pass_info = user_pass + pass_purpose + pass_username
 
 
-def lita(user_pass, pass_purpose, pass_username):
-    format_data(user_pass, pass_purpose, pass_username)
-    
+
+def format_data(pass_info):
+    pass_info = []
+    return [pass_info[i:i+4] for i in range(0, len(pass_info), 4)]
+
+def encrypt_block(pass_info):
+    assert len(pass_info) == 16 
+
+    matrix = format_data(pass_info)
+
+    print(matrix)
+
+encrypt_block(pass_info)
