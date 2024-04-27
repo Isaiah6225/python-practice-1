e4cd69e8ba77dba21f7f0dc584831e34b24c9ef2
Isaiah6225 <isaiahjohnson6225@gmail.com>
2024-04-27 00:08:54 -0400

    Added padding functionality to algorithm

--- password_manager_rpi/lita.py
+++ password_manager_rpi/lita.py
@@ -3,19 +3,57 @@ import sys
 user_pass = 'abcdefGHIJKL1234@#'
 pass_purpose = 'Google'
 pass_username = 'Isaiah6225'
-pass_info = user_pass + pass_purpose + pass_username
 
 
 
-def format_data(pass_info):
+
+def format_data(user_pass, pass_purpose, pass_username):
     pass_info = []
-    return [pass_info[i:i+4] for i in range(0, len(pass_info), 4)]
+    
+    pass_info = user_pass + pass_purpose + pass_username
+    ex = [pass_info[i:i+4] for i in range(0, len(pass_info), 4)]
+    return ex
 
-def encrypt_block(pass_info):
-    assert len(pass_info) == 16 
+# generates padding if user block isn't 16 bytes 
+def generate_padding(pass_info):
+    num_of_bytes_pad = 16 % len(pass_info)
+    padding = []
 
-    matrix = format_data(pass_info)
+    for x in range(1, num_of_bytes_pad + 1):
+        padding.append(bytes(x))
+
+    return pass_info.extend(padding)
+
+    
+    
+#take data from format data to arrange the data into a matrix
+
+def arrange_matrix():
+
+    pass_info = format_data(user_pass, pass_purpose, pass_username)
+    generate_padding(pass_info)
+
+    matrix = []
+    num_rows_defined = 4
+    cols = 4
+
+    #Populate the matrix from pass_info
+    for i in range(num_rows_defined):
+        row = []
+        for j in range(cols):
+            #Calculates the index in pass info to the element in the matrix
+            index = i * cols + j
+
+            #if index is within bounds of the list append the value to the row
+            if index < len(pass_info):
+
+                row.append(pass_info[index])
+
+            #If the index exceeds the length fo the list add padding
+            else: 
+                row.append(None)
+        matrix.append(cols)
 
     print(matrix)
 
-encrypt_block(pass_info)
+arrange_matrix()
